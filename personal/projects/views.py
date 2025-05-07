e0a493ad65b57b4187ac58ca1313ee481d34d006
Kwanele Myeza <gcinakahle.myeza@gmail.com>
2025-05-07 22:55:42 +0200

    Added foooter, linked github and linkedin, added temp projects page, and updated resumme content.

--- personal/projects/views.py
+++ personal/projects/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 
 # Create your views here.
 def projects_home(request):
-    return render(request, 'projects/home.html')
+    return render(request, 'projects/projects.html')
